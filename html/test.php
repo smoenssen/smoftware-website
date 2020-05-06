df491722378e80c84c438481caac9b86404052f2
smoenssen <steve.moenssen@gmail.com>
2020-05-06 18:33:48 -0400

    Initial commit

---
+++ html/test.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "st3v37895";
+$dbname = "test";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO user_info (Name, Age)
+    VALUES ('Steve Moenssen', '56')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
---
+++ html/welcome.php
@@ -0,0 +1,33 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<style> .content { max-width: 500px; margin: auto; } </style>
+<body>
+    <div class="content">
+        <div class="wrapper">
+            <h2>Welcome!</h2>
+            <p><a href="logout.php">Logout</a>.</p>
+        </div>
+    </div>
+</body>
+</html>
